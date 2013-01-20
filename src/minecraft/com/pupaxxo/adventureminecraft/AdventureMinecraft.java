bcbb5e6d686dac37ccfa7f1ad160fe50573a2e82
Andrea Ruggiero <pupax@outlook.com>
2013-01-20 19:32:16 +0100

    Added modinfo to the main class

--- src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
+++ src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
@@ -1,5 +1,10 @@
 package com.pupaxxo.adventureminecraft;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "AdventureMinecraft_pupaxxo", name = "AdventureMinecraft", version = "0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class AdventureMinecraft {
 
 }
