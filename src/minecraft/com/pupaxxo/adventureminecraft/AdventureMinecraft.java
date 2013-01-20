02600ae6ba0cb6cf582802759741d32d7a56b9e3
Andrea Ruggiero <pupax@outlook.com>
2013-01-20 19:48:18 +0100

    Add preinit function

--- src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
+++ src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
@@ -2,12 +2,18 @@ package com.pupaxxo.adventureminecraft;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "AdventureMinecraft_pupaxxo", name = "AdventureMinecraft", version = "0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class AdventureMinecraft {
+	@PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+		
+	}
 	@Init
 	public void load(FMLInitializationEvent event) 
 	{
