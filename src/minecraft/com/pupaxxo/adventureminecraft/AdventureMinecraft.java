1c870b7721533c4cbd6324541d679722063fbb26
Andrea Ruggiero <pupax@outlook.com>
2013-01-20 21:08:10 +0100

    more about configs

--- src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
+++ src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
@@ -1,7 +1,7 @@
 package com.pupaxxo.adventureminecraft;
 
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.Property;
+import com.pupaxxo.adventureminecraft.config.Config;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PreInit;
@@ -12,17 +12,14 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = "AdventureMinecraft_pupaxxo", name = "AdventureMinecraft", version = "0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)
 public class AdventureMinecraft {
+	public Object[] configs;
 	public boolean checkUpdates;
 	@PreInit
     public void preInit(FMLPreInitializationEvent event) 
 	{
-		//Create configuration file 
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
-        Property checkup = config.get(Configuration.CATEGORY_GENERAL, "Check updates", true);
-        checkup.comment = "True fot check if there are new version of the mods , false if you don't want the notify.";
-        checkUpdates = checkup.getBoolean(true);
-		config.save();
+		//Load configuration file 
+		configs = Config.load(event);
+		checkUpdates = Config.toBoolean(configs[0].toString());
 	}
 	
 	@Init
---
+++ src/minecraft/com/pupaxxo/adventureminecraft/config/Config.java
@@ -0,0 +1,28 @@
+package com.pupaxxo.adventureminecraft.config;
+
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Config {
+	public Config() {
+		
+	}
+	public static Object[] load(FMLPreInitializationEvent event) {
+		Object[] configs = new Object[100];
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+        Property checkup = config.get(Configuration.CATEGORY_GENERAL, "Check updates", true);
+        checkup.comment = "True fot check if there are new version of the mods , false if you don't want the notify.";
+        configs[0] = checkup.getBoolean(true);
+		config.save();
+		return configs;
+	}
+	public static Boolean toBoolean(String text) {
+		if (text.equals("true")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
