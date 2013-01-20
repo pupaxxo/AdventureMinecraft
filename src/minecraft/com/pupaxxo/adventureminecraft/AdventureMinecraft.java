f91294cd15cfdfa17d91aa8b08b90746b6c0a7ee
Andrea Ruggiero <pupax@outlook.com>
2013-01-20 19:44:50 +0100

    added load function to main class

--- src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
+++ src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
@@ -1,10 +1,16 @@
 package com.pupaxxo.adventureminecraft;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "AdventureMinecraft_pupaxxo", name = "AdventureMinecraft", version = "0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class AdventureMinecraft {
-
+	@Init
+	public void load(FMLInitializationEvent event) 
+	{
+	
+	}
 }
