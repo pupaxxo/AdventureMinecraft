f2a6dd803a010274d6f2114a47dcc857da27da43
Andrea Ruggiero <pupax@outlook.com>
2013-01-22 16:19:54 +0100

    Revert "stupid commit"
    This reverts commit 0bbcfc11366718de4ca8008ed2d7ee8e36bec938.

--- src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
+++ src/minecraft/com/pupaxxo/adventureminecraft/AdventureMinecraft.java
@@ -7,34 +7,25 @@ import com.pupaxxo.adventureminecraft.config.Config;
 import com.pupaxxo.adventureminecraft.core.Reference;
 import com.pupaxxo.adventureminecraft.crafting.CraftingManager;
 import com.pupaxxo.adventureminecraft.creativetab.CreativeTabAM;
-import com.pupaxxo.adventureminecraft.handler.GuiHandler;
-import com.pupaxxo.adventureminecraft.handler.PacketHandler;
+import com.pupaxxo.adventureminecraft.dimension.Dimension;
 import com.pupaxxo.adventureminecraft.item.Items;
 import com.pupaxxo.adventureminecraft.proxy.CommonProxy;
-import com.pupaxxo.adventureminecraft.tile.TileModernCraftingTable;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = Reference.modid, name = Reference.modname, version = Reference.version)
-@NetworkMod(clientSideRequired = Reference.clientSideRequired, serverSideRequired = Reference.serverSideRequired , channels={"GenericRandom"}, packetHandler = PacketHandler.class)
-
+@NetworkMod(clientSideRequired = Reference.clientSideRequired, serverSideRequired = Reference.serverSideRequired)
 public class AdventureMinecraft {
 	@SidedProxy(clientSide = Reference.clientProxy, serverSide = Reference.commondProxy)
 	
 	public static CommonProxy proxy;
-	private GuiHandler guiHandler = new GuiHandler();
-	@Instance
-	public static AdventureMinecraft instance = new AdventureMinecraft();
 	public Object[] configs;
 	public boolean checkUpdates;
 	public static CreativeTabs tabsAM = new CreativeTabAM(CreativeTabs.getNextID(), Reference.modid);
@@ -50,8 +41,6 @@ public class AdventureMinecraft {
 	@Init
 	public void load(FMLInitializationEvent event) 
 	{
-		NetworkRegistry.instance().registerGuiHandler(this, guiHandler);
-		GameRegistry.registerTileEntity(TileModernCraftingTable.class, "tileModernCraftingTable");
 		//Register lang for creative tabs
 		LanguageRegistry.instance().addStringLocalization("itemGroup." + Reference.modid, "en_US", "Adventure Minecraft");
 		//Register proxy
